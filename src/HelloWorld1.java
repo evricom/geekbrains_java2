80a806cfcf528df781d1dcf5911e14ba27ebb04d
Dmitriy Staroverov <dima@evricom.net>
2018-07-15 23:30:09 +0400

    Merge NewBranch

--- src/HelloWorld1.java
+++ src/HelloWorld1.java
@@ -2,5 +2,6 @@ public class HelloWorld1 {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("HelloMerge");
+        System.out.println("Merge NewBranch");
     }
 }
