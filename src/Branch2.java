562ee47990f3bf63af71f3e36cf08030eb01fae9
Dmitriy Staroverov <dima@evricom.net>
2018-07-16 02:27:40 +0400

    Branch2__22 add

--- src/Branch2.java
+++ src/Branch2.java
@@ -2,5 +2,6 @@ public class Branch2 {
     public static void main(String[] args) {
         System.out.println("Branch2");
         System.out.println("Branch2__");
+        System.out.println("Branch2__22");
     }
 }
