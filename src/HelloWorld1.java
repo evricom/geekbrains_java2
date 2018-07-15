81780901fb1303f9dfe9f21a02b581e6429735e9
Dmitriy Staroverov <dima@evricom.net>
2018-07-16 03:23:49 +0400

    clear poject

--- src/HelloWorld1.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld1 {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("HelloMerge");
-        System.out.println("Merge NewBranch2");
-        System.out.println("Branch2 add");
-    }
-}
