d4f83cc90717a64bf99f5de3b16df17d1ffbaf28
Dmitriy Staroverov <dima@evricom.net>
2018-07-16 03:24:48 +0400

    clear poject

--- src/Branch2.java
+++
@@ -1,7 +0,0 @@
-public class Branch2 {
-    public static void main(String[] args) {
-        System.out.println("Branch2");
-        System.out.println("Branch2__");
-        System.out.println("Branch2__22");
-    }
-}
