e5affd08fec35ab4d2b59e393afc0c63dfb9ecf7
Dmitriy Staroverov <dima@evricom.net>
2018-07-16 03:45:43 +0400

    lesson5 start

--- src/Lesson5/MainThread.java
+++ src/Lesson5/MainThread.java
@@ -2,6 +2,24 @@ package Lesson5;
 
 public class MainThread {
     public static void main(String[] args) {
+        MyThread myThread_1 = new MyThread();
+        MyThread myThread_2 = new MyThread();
+        myThread_1.start();
+        myThread_2.start();
+    }
+}
 
+class MyThread extends Thread{
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10 ; i++) {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(i);
+        }
     }
 }
